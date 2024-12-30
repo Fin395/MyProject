27bc9e8e23b867d569614aa6b344278f772db152
sergei finogenov <serfinogenov@yandex.ru>
2024-12-31 02:11:47 +0300

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(list_of_dicts: list, state="EXECUTED") -> list:
-    """Отбирает словари по соответствию ключа указанному значению"""
+    """Отбирает те транзакции, которые были исполнены"""
     new_list = []
     for dictionary in list_of_dicts:
         if "EXECUTED" in dictionary.values():
@@ -9,6 +9,20 @@ def filter_by_state(list_of_dicts: list, state="EXECUTED") -> list:
 
 # пример входных данных и работы функции
 # result = filter_by_state([
+# {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+# {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+# {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+# {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+# print(result)
+
+
+def sort_by_date(list_of_dicts: list, reverse=True) -> list:
+    """Сортирует транзакции в порядке убывания по дате их осуществления"""
+    return sorted(list_of_dicts, key=lambda x: x["date"], reverse=reverse)
+
+
+# пример входных данных и работы функции
+# result = sort_by_date([
 # {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 # {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 # {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
