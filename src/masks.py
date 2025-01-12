d5c4077b031a165929fa4ac5a66f2951a91ebdac
Sergei Finogenov <serfinogenov@yandex.ru>
2025-01-13 02:44:21 +0300

    commit tests for modules test_widget.py and test_processing.py, as well as update for modules widget.py and processing.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция,которая принимает на вход номер карты и возвращает ее маску"""
     card_number_length = len(card_number)
-    if card_number_length !=16:
+    if card_number_length != 16:
         raise ValueError("Номер карты должен состоять из 16 цифр")
     else:
         splited_card_number = card_number.split()
--- src/widget.py
+++ src/widget.py
@@ -3,24 +3,33 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(card_or_account_data: str) -> str:
     """Функция, которая обрабатывает информацию о картах и счетах"""
-    if card_or_account_data != "":
+    try:
         splited_card_or_account_data = card_or_account_data.split()
         number = splited_card_or_account_data[-1]
+    except IndexError:
+        print("Вы забыли ввести номер карты или счета")
+
+    else:
         if "счет" in card_or_account_data.lower():
             masked_data = card_or_account_data.replace(number, get_mask_account(number))
         else:
             masked_data = card_or_account_data.replace(number, get_mask_card_number(number))
         return masked_data
-    else:
-        raise ValueError("Вы забыли ввести номер")
 
 
 def get_date(date_to_fix: str) -> str:
     """Функция возвращает дату в корректном формате"""
     date_list = []
-    sliced_date = date_to_fix[0:10]
-    sliced_date_splited = sliced_date.split("-")
-    for number in sliced_date_splited[::-1]:
-        date_list.append(number)
-    date_in_required_format = ".".join(date_list)
-    return date_in_required_format
+    try:
+        if date_to_fix != "":
+            sliced_date = date_to_fix[0:10]
+            sliced_date_splited = sliced_date.split("-")
+            for number in sliced_date_splited[::-1]:
+                date_list.append(number)
+            date_in_required_format = ".".join(date_list)
+        else:
+            raise ValueError
+    except ValueError:
+        print("Вы забыли ввести дату")
+    else:
+        return date_in_required_format
